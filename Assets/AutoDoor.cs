63b9a2f79b17dcaa8b52aa1e91c62f3f7c10fec0
myeong chul <10jade0070@gmail.com>
2018-12-17 12:46:17 +0900

    자동문 및 형광등 추가

---
+++ Assets/AutoDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AutoDoor : MonoBehaviour
+{
+    public GameObject leftDoor;
+    //public GameObject rightDoor;
+
+    private float originalLeftDoorX;
+    //private float originalRightDoorX;
+
+
+
+    void Start()
+    {
+        originalLeftDoorX = leftDoor.transform.position.x;
+        //originalRightDoorX = rightDoor.transform.position.x;
+    }
+
+    void OpenDoor()
+    {
+        //rightDoor.transform.DOMoveX(originalRightDoorX + 5f,1f);
+        leftDoor.transform.DOMoveX(originalLeftDoorX - 7f,1f);
+    }
+
+    void CloseDoor()
+    {
+        //rightDoor.transform.DOMoveX(originalRightDoorX , 1f);
+        leftDoor.transform.DOMoveX(originalLeftDoorX , 1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CloseDoor();
+        }
+    }
+
+}
